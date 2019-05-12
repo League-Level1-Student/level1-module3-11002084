79418184a808af2d2ec7aed44133f3428b01aecb
League Student <league@AdministorsiMac.attlocal.net>
2019-05-12 16:00:10 -0700

    Movie Theater

---
+++ src/netflix/MovieTheater.java
@@ -0,0 +1,16 @@
+package netflix;
+
+public class MovieTheater {
+	public static void main(String[] args) {
+		Movie shrek = new Movie("Sherk", 100); //OOOOHHHHHHH OOOOOOOOOOOOOOOOOHHHHHHHHHHH that's too hot
+		Movie bee = new Movie("Bee Movie", 99); //Thinking bee
+		Movie pikachu = new Movie("Detective Pikachu", 13); //Pika pika
+		Movie avengers = new Movie("Avengers: Endgame", 11); //You spoil, you die
+		Movie fish = new Movie("Finding Nemo", 8); //Fishy Friends, not food
+		Movie lego = new Movie("Lego Movie", 8); //It's pretty awesome
+		Movie furries = new Movie("Zootopia", 5); //Oh Oh Oh my god, try to run
+		Movie emoji = new Movie("Emoji Movie", -5); //That seems like an ad with extra steps
+		Movie sonic = new Movie("Live Action Sonic", -123); // Holy SHIIIIIIII-
+		Movie cat= new Movie("Cat in the Hat Live Action", -200); //RUN RUN RUN RUN RUN RUN RUN
+	}
+}
